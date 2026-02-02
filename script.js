4518831f26a13c2994b80e9e86e48f85aa120d45
Evridiki Lam <evridiki.lam@gmail.com>
2026-02-02 12:42:03 +0200

    Create the book object

--- script.js
+++ script.js
@@ -1,4 +1,20 @@
 //If there are saved books in array load them, if not empty array
 let books = JSON.parse(localStorage.getItem("books")) || [];
 
-// const form = document.querySelector(#bookForm);
+const form = document.querySelector("#bookForm");
+
+form.addEventListener("submit", function(e) {
+  e.preventDefault();
+
+  const book = {
+    id: Date.now(),
+    title: document.querySelector("#btitle").value,
+    author: document.querySelector("#bauthor").value,
+    status: document.querySelector("#status").value,
+    rating: document.querySelector("#rating").value,
+    notes: document.querySelector("#notes").value
+  };
+
+  books.push(book);
+  saveBooks();
+});
